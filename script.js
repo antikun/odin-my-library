c5c8112bc7ea0a05571e88e565f0a182a8099a5b
antikun <121240743+antikun@users.noreply.github.com>
2023-03-18 21:02:02 +0200

    Add logic for book submit button

--- script.js
+++ script.js
@@ -9,12 +9,27 @@ class Book {
 }
 
 const userInput = {
-    title: '',
-    author: '',
-    pages: '',
-    status: '',
-    rating: '',
+    title: "",
+    author: "",
+    pages: "",
+    status: "",
+    rating: "",
     library: [],
+
+    update() {
+        this.title = document.querySelector("#title").value;
+        this.author = document.querySelector("#author").value;
+        this.pages = document.querySelector("#pages").value;
+        this.rating = document.querySelector("#rating").value;
+
+        const radioBtns = document.querySelectorAll(".status-btn");
+        for (let i = 0; i < radioBtns.length; i++) {
+            if (radioBtns[i].checked) {
+                this.status = radioBtns[i].labels[i].textContent;
+                break;
+            }
+        }
+    },
 
     addBookToLibrary() {
         const { title, author, pages, status, rating, library } = this;
@@ -22,5 +37,19 @@ const userInput = {
         library.push(newBook);
     }
 }
+
+function setEventListeners(e) {
+    document.querySelector("#submit-book-btn").addEventListener("click", (e) => {
+        e.preventDefault();
+        userInput.update();
+        userInput.addBookToLibrary();
+    })
+}
+
+function main() {
+    setEventListeners();
+}
+
+main();
 
 
