93d75d1666f475541eee23a9edd4bebbdc5ef9f6
antikun <121240743+antikun@users.noreply.github.com>
2023-03-18 22:38:04 +0200

    Add more functionality

--- script.js
+++ script.js
@@ -23,9 +23,9 @@ const userInput = {
         this.rating = document.querySelector("#rating").value;
 
         const radioBtns = document.querySelectorAll(".status-btn");
-        for (let i = 0; i < radioBtns.length; i++) {
-            if (radioBtns[i].checked) {
-                this.status = radioBtns[i].labels[i].textContent;
+        for (const radioBtn of radioBtns) {
+            if (radioBtn.checked) {
+                this.status = radioBtn.value;
                 break;
             }
         }
@@ -39,10 +39,24 @@ const userInput = {
 }
 
 function setEventListeners(e) {
+    // INPUT FORM POPUP
+    const addBookBtn = document.querySelector(".add-book-btn");
+
+    function toggleDisplay() {
+        document.querySelector(".form-container").classList.toggle("no-display");
+        addBookBtn.classList.toggle("no-display");
+    }
+
+    addBookBtn.addEventListener("click", toggleDisplay);
+
+    //SUBMIT BOOK 
+
     document.querySelector("#submit-book-btn").addEventListener("click", (e) => {
         e.preventDefault();
+
         userInput.update();
         userInput.addBookToLibrary();
+        toggleDisplay();
     })
 }
 
